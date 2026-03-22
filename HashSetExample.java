75f355f35fc719983ddae11a4aa5e7cd27863c15
Vaibhav Korake <vaibhavkorake182@gmail.com>
2026-03-22 09:19:48 +0530

    Basic HashSet syntax

---
+++ HashSetExample.java
@@ -0,0 +1,20 @@
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashSetExample{
+    public static void main(String[] args) {
+        
+        Set<String> set = new HashSet<>();
+        
+        set.add("Vaibhav");
+        set.add("Shreya");
+        set.add("Atharv");
+        set.add("Prasad");
+        set.add("Yash");
+        set.add("Parth");
+        set.add("Prajwal");
+
+        System.out.println(set);
+    }
+}
