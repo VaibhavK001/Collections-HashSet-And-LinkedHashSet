4bcfe1299932915555aef9db84450969c0babba5
Vaibhav Korake <vaibhavkorake182@gmail.com>
2026-03-22 09:32:21 +0530

    HashSet methods practice code

---
+++ HashSetMethods.java
@@ -0,0 +1,27 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashSetMethods{
+    public static void main(String[] args) {
+        
+        Set<Integer> set = new HashSet<>();
+
+        set.add(10);
+        set.add(20);
+        set.add(30);
+        set.add(40);
+        set.add(50);
+        set.add(50);
+
+        System.out.println(set);
+        
+        set.remove(10);
+        System.out.println(set);
+
+        System.out.println(set.size());
+
+        System.out.println(set.contains(30));
+
+        System.out.println(set.isEmpty());
+    }
+}
