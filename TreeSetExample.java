09e36112fb2cd8e665d4e888d238634819b6250e
Vaibhav Korake <vaibhavkorake182@gmail.com>
2026-03-22 11:38:50 +0530

    TreeSet practice example

---
+++ TreeSetExample.java
@@ -0,0 +1,17 @@
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class TreeSetExample{
+    public static void main (String[] args){
+
+            Set<Integer> set = new TreeSet<>();
+            set.add(9);
+            set.add(6);
+            set.add(11);
+            set.add(2);
+
+            System.out.println(set);
+
+    }
+}
